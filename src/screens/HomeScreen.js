d311bf9e8d61b8079d4f7da59bfd7b50390d733b
oscar <oscaroceguera@gmail.com>
2020-04-02 18:58:20 -0700

    Navigation Users Between Screens

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,23 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Oscar oceguera</Text>;
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>Oscar oceguera</Text>
+      <Button
+        onPress={() => navigation.navigate("Components")}
+        title="Go to Commponents Demo"
+      />
+      <Button
+        onPress={() => navigation.navigate("List")}
+        title="Go to List Demo"
+      />
+      {/* <TouchableOpacity onPress={() => navigation.navigate("List")}>
+        <Text>Go to list Demo</Text>
+      </TouchableOpacity> */}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
